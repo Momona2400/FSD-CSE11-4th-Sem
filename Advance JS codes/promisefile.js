e75a9b1655b40f34c67f8da4e3af91a9d624dc44
Momona <ananyaanuj2006@gmail.com>
2026-03-16 15:38:01 +0530

    promise file

--- Advance JS codes/promisefile.js
+++ Advance JS codes/promisefile.js
@@ -1,11 +1,16 @@
 //promise file
 
-let user={
-    name:"John",
-    age:30,
-    address:{
-        city:"New York",
-        country:"USA"
+let promise=new Promise(function(resolve,reject){
+    if(true){
+        resolve("task done")
     }
-};
+    else{
+        reject("task failed")
+    }
+});
 
+promise.then(function(result){
+    console.log(result) // task done
+}).catch(function(error){
+    console.log(error) // task failed
+});
